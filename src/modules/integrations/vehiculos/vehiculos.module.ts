bad61e57298f129c8d8527868378fff6e894abbe
Jose Luis Prado Valencia <joseluispradov046@gmail.com>
2025-12-01 20:32:03 -0500

    feat(rutas): agregar rutas externas de la API con documentación Swagger

--- src/modules/integrations/vehiculos/vehiculos.module.ts
+++ src/modules/integrations/vehiculos/vehiculos.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { VehiculosService } from './vehiculos.service';
 import { VehiculosController } from './vehiculos.controller';
-import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
     imports: [AuthModule],
--- src/seeders/users-seeder/users-seeder.service.ts
+++ src/seeders/users-seeder/users-seeder.service.ts
@@ -11,19 +11,19 @@ export class UsersSeederService {
         private readonly userRepository: Repository<User>,
 
         private readonly usersService: UsersService
-    ){}
+    ) { }
 
-    async run(){
+    async run() {
         await this.userRepository.clear()
 
         const users = [
-            {nameuser: 'admin', password: 'admin1234'},
-            {nameuser: 'jose', password: 'jose1234'},
-            {nameuser: 'heiner', password: 'heiner1234'},
-            {nameuser: 'jonatan', password: 'jonatan1234'},
+            { nameuser: 'admin', password: 'admin1234' },
+            { nameuser: 'jose', password: 'jose1234' },
+            { nameuser: 'heiner', password: 'heiner1234' },
+            { nameuser: 'jonatan', password: 'jonatan1234' },
         ]
         for (const u of users) {
-      await this.usersService.create(u);
-    }
+            await this.usersService.create(u);
+        }
     }
 }
